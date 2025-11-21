b63e4c926c940c78974c6e784dd9eb0c059dfa54
yuxin jiang <154605082+yuxin-jiang@users.noreply.github.com>
2025-11-22 00:38:53 +0800

    Add files via upload

--- Anomagic_train.py
+++ Anomagic_train.py
@@ -271,7 +271,7 @@ def parse_args():
     parser.add_argument(
         "--data_json_file",
         type=str,
-        default="Datasets/merged_ad_datasets_cover1_copped.json",
+        default="Datasets/merged_ad_datasets.json",
         help="Training data JSON file path",
     )
     parser.add_argument(
@@ -289,7 +289,7 @@ def parse_args():
     parser.add_argument(
         "--output_dir",
         type=str,
-        default="sd-anomagic_short_text_cover1_copped",
+        default="sd-anomagic",
         help="The output directory where the model predictions and checkpoints will be written.",
     )
     parser.add_argument(
